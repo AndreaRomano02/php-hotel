8a92b7012e154b5508eaba70e916b00bb17a7cbc
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-18 15:21:20 +0200

    Add table with bootstrap

--- index.php
+++ index.php
@@ -45,26 +45,49 @@ $hotels = [
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' integrity='sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==' crossorigin='anonymous' />
+  <link rel="stylesheet" href="style.css">
   <title>Hotels</title>
 </head>
 
 <body>
-  <?php foreach ($hotels as $hotel) : ?>
-    <h2><?= $hotel['name'] ?></h2>
-    <p><?= $hotel['description'] ?></p>
-    <span><strong>Parcheggio: </strong></span>
-    <?php if ($hotel['parking']) : ?>
-      <span>Disponibile.</span>
-    <?php else : ?>
-      <span>Non Disponibile.</span>
-    <?php endif ?>
-    <p>Vote: <?= $hotel['vote'] ?></p>
-    <p>
-      <span>Distanza dal centro: <strong><?= $hotel['distance_to_center'] ?></strong></span>
-    </p>
+  <header>
+    <h1>HOTELS</h1>
+  </header>
+  <main>
+    <table class="table table-dark table-striped-columns">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Descrizione</th>
+          <th>Parcheggio</th>
+          <th>Voto</th>
+          <th>Distanza</th>
+        </tr>
+      </thead>
+      <tbody>
 
-  <?php endforeach ?>
+        <?php foreach ($hotels as $hotel) : ?>
+          <tr>
+            <td><?= $hotel['name'] ?></td>
+            <td><?= $hotel['description'] ?></td>
+            <td>
+              <?php if ($hotel['parking']) : ?>
+                <span>Disponibile</span>
+              <?php else : ?>
+                <span>Non Disponibile</span>
+              <?php endif ?>
+            </td>
+            <td>Vote: <?= $hotel['vote'] ?></td>
+            <td>
+              <span><?= $hotel['distance_to_center'] ?></span>
+            </td>
+          </tr>
+        <?php endforeach ?>
 
+      </tbody>
+    </table>
+  </main>
 </body>
 
 </html>
