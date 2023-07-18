7aaecf74378ba1e7c73244cc0341913b2eec88de
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-18 15:59:00 +0200

    Add filter by vote

--- index.php
+++ index.php
@@ -39,6 +39,7 @@ $hotels = [
 ];
 
 $park = $_GET['park'] ?? false;
+$vote = $_GET['vote'] ?? '';
 ?>
 
 <!DOCTYPE html>
@@ -59,6 +60,13 @@ $park = $_GET['park'] ?? false;
     <form action="index.php" class="text-start mt-4 ms-5">
       <label for="park">Parcheggio</label>
       <input type="checkbox" name="park" id="park" class="form-check-input mt-2">
+      <label class="ms-3" for="vote">Filtra per voto</label>
+      <select name="vote" id="vote">
+        <option value="">--</option>
+        <?php for ($n = 1; $n <= 5; $n++) : ?>
+          <option value="<?= $n ?>"><?= $n ?></option>
+        <?php endfor ?>
+      </select>
       <button class="btn btn-light ms-3">Filtra</button>
       <a class="btn btn-danger " href="index.php">Rimuovi Filtri</a>
     </form>
@@ -78,6 +86,9 @@ $park = $_GET['park'] ?? false;
 
         <?php foreach ($hotels as $hotel) : ?>
           <?php if ($park == 'on' && !$hotel['parking']) : ?>
+            <?php continue; ?>
+          <?php endif ?>
+          <?php if ($vote > $hotel['vote']) : ?>
             <?php continue; ?>
           <?php endif ?>
           <tr>
